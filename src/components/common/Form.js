42ee6a4a4d183c5dddcd5b394b6d2296f5e3dfae
3ba2ii <aghonem2011@gmail.com>
2021-01-21 18:55:38 +0200

    🥳 : Added the visibility button and logic to the password input field

--- src/components/common/Form.js
+++ src/components/common/Form.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './form.css';
 
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+
 export const Form = (props) => {
+  const [visiblePassword, setVisiblePassword] = useState(false);
   return (
     <div className={`${props.className}-form`}>
+      <div className='visible'>
+        {props.type === 'password' &&
+          (!visiblePassword ? (
+            <span
+              className='cursor-pointer'
+              onClick={(e) => setVisiblePassword(true)}
+            >
+              <VisibilityIcon className='visibility-icon' fontSize={'small'} />
+            </span>
+          ) : (
+            <span
+              className='cursor-pointer'
+              onClick={(e) => setVisiblePassword(false)}
+            >
+              <VisibilityOffIcon
+                className='visibility-icon'
+                fontSize={'small'}
+              />
+            </span>
+          ))}
+      </div>
       <input
-        type={props.type}
+        type={
+          props.type === 'text' ? 'text' : visiblePassword ? 'text' : 'password'
+        }
         name={props.name}
         required={props.required || true}
         onChange={(e) => {
@@ -13,6 +40,7 @@ export const Form = (props) => {
         }}
         autoComplete='off'
       />
+
       <label htmlFor={props.name} className={`label-name`}>
         <span className='content-name'>{props.name}</span>
       </label>

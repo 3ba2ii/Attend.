455f40b4be6054ff11cc325a357f9e1ff0ef2df7
3ba2ii <aghonem2011@gmail.com>
2021-01-30 23:23:14 +0200

    fixed some alt issue for (SEO)

--- src/components/Dashboard/CourseCard.js
+++ src/components/Dashboard/CourseCard.js
@@ -16,7 +16,7 @@ const CourseCard = ({
     <div className='course-card-container card-shadow'>
       <div className='center-area'>
         <header className='course-info'>
-          <img src={courseImageURL} alt={'course-image'} />
+          <img src={courseImageURL} alt={'course-img'} />
           <div
             className='course-name'
             style={{ whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}

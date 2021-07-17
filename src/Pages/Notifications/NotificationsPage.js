f5280c97f2167d50c36990d961716c91909bc410
3ba2ii <aghonem2011@gmail.com>
2021-07-17 06:10:30 +0200

    delete pages folder

--- src/Pages/Notifications/NotificationsPage.js
+++
@@ -1,9 +0,0 @@
-const NotificationsPage = () => {
-  return (
-    <main className='notifications-page'>
-      <header>
-        <h3>Notifications Center</h3>
-      </header>
-    </main>
-  );
-};
